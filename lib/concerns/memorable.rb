ad54e4e16ced39046460f7e920b955ce0c10a5c6
Lisa Feng <lisafeng10@gmail.com>
2020-07-02 18:24:34 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,15 @@
 module Memorable
-  def reset_all
-    self.all.clear
-  end
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def count
-    self.all.count
+    def count
+      self.all.count
+    end
+  end
+  
+  module InstanceMethods
+    
   end
 end
